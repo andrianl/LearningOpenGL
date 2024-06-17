121b3ea12c3e253a28be55fc1ce6e77cd04cb994
Lutsu Andrian <lutsu.andrian@gmail.com>
2024-06-17 21:50:21 +0300

    Readme

--- Application/Source/main.cpp
+++ Application/Source/main.cpp
@@ -33,7 +33,7 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "Learn OpenGL", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
